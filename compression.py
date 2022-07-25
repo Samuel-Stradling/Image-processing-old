a0a6bfa7b604d7787fe5f03c4912789d098e2b40
Macbook air <sammystradling2006@gmail.com>
2022-07-25 21:49:41 +0100

    made code more readable with more descriptive var name

--- compression.py
+++ compression.py
@@ -1,16 +1,17 @@
 from PIL import Image
 def compress(directory):
     # set the base width of the result
-    basewidth = 300
+    widthRescale = 300 #change to control rescaling of image
     img = Image.open(directory)
     # determining the height ratio
-    wpercent = (basewidth/float(img.size[0]))
+    wpercent = (widthRescale/float(img.size[0]))
     hsize = int((float(img.size[1])*float(wpercent)))
     # resize image and save
-    img = img.resize((basewidth,hsize), Image.Resampling.LANCZOS)
+    img = img.resize((widthRescale,hsize), Image.Resampling.LANCZOS)
     lastslash = directory.rfind("/") + 1
     directory = list(directory)
     directory.insert(lastslash, "C")
     directory = ''.join(directory)
     img.save(directory)
-compress("/Users/sam/Pictures/mac tonight/2.jpg")
+    return directory
+#compress("/Users/sam/Pictures/mac tonight/2.jpg")
