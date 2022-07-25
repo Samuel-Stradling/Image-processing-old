474c0ff2d3911cea11fcaadf317f89f1e12426bb
Macbook air <sammystradling2006@gmail.com>
2022-07-25 21:40:06 +0100

    working resize wihout deprication

--- compression.py
+++ compression.py
@@ -7,10 +7,10 @@ def compress(directory):
     wpercent = (basewidth/float(img.size[0]))
     hsize = int((float(img.size[1])*float(wpercent)))
     # resize image and save
-    img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+    img = img.resize((basewidth,hsize), Image.Resampling.LANCZOS)
     lastslash = directory.rfind("/") + 1
     directory = list(directory)
     directory.insert(lastslash, "C")
     directory = ''.join(directory)
     img.save(directory)
-compress("/Users/sam/Pictures/3.jpg")
+compress("/Users/sam/Pictures/mac tonight/2.jpg")
