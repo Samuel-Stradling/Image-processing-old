1d17448ab7be528cb7236cfcb2d8636538ab83a0
Macbook air <sammystradling2006@gmail.com>
2022-07-30 20:20:56 +0100

    made program into func

--- video.py
+++ video.py
@@ -5,37 +5,39 @@ import os
 import pygame
 
 #LOOK INTO THREADING TO CONTROL SPEED OF ANALYSIS
+def video_color():
+    pygame.init()
+    screen = pygame.display.set_mode((500, 700))
+    pygame.display.set_caption("Avg Color")
+    clock = pygame.time.Clock()
+    done = False
 
-pygame.init()
-screen = pygame.display.set_mode((500, 700))
-pygame.display.set_caption("Avg Color")
-clock = pygame.time.Clock()
-done = False
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
 
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+        clock.tick(60)
 
-    clock.tick(60)
-
-    vidcap = cv2.VideoCapture("test.mp4")
-    count = 0
-    while success:
-        #LOOK INTO THREADING TO WAIT FOR EACH FRAME TO BE SCANNED CONSECUTIVELY
-        filename = "/Users/sam/Programming/Image-processing/frames/frame%d.jpg" % count
-        cv2.imwrite(filename, image)
-        if count % 8 == 0:
-    
-            compress(filename)
-            print(f"\nFrame {count}")
-            rgb = get_color(filename)
-            screen.fill(rgb)
-            pygame.display.flip()
-
-        os.remove(filename)
+        vidcap = cv2.VideoCapture("test.mp4")
         success, image = vidcap.read()
-        count += 1
-    
+        count = 0
+        while success:
+            #LOOK INTO THREADING TO WAIT FOR EACH FRAME TO BE SCANNED CONSECUTIVELY
+            filename = "/Users/sam/Programming/Image-processing/frames/frame%d.jpg" % count
+            cv2.imwrite(filename, image)
+            if count % 8 == 0:
+        
+                compress(filename)
+                print(f"\nFrame {count}")
+                rgb = get_color(filename)
+                screen.fill(rgb)
+                pygame.display.flip()
 
-pygame.quit()
+            os.remove(filename)
+            success, image = vidcap.read()
+            count += 1
+        
+
+    pygame.quit()
+video_color()
