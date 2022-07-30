635aa15ec70e616dc397d5316e0c09753558aa9a
Macbook air <sammystradling2006@gmail.com>
2022-07-30 19:32:32 +0100

    optimised compression for video

---
+++ compressionForVideo.py
@@ -0,0 +1,13 @@
+from PIL import Image
+def compress(directory):
+    # set the base width of the result
+    widthRescale = 300 #change to control rescaling of image
+    img = Image.open(directory)
+    # determining the height ratio
+    wpercent = (widthRescale/float(img.size[0]))
+    hsize = int((float(img.size[1])*float(wpercent)))
+    # resize image and save
+    img = img.resize((widthRescale,hsize), Image.Resampling.LANCZOS)
+    img.save(directory)
+    return directory
+#compress("/Users/sam/Pictures/mac tonight/2.jpg")
