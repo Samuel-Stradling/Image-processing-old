5b397728b7947789cc1eb8dff374839a3149f6e2
Macbook air <sammystradling2006@gmail.com>
2022-07-30 19:32:13 +0100

    effective compression and scanning of every 8th frame

--- video.py
+++ video.py
@@ -1,5 +1,5 @@
 import cv2
-from compression import compress
+from compressionForVideo import compress
 from colorForVideo import get_color
 import os
 
@@ -8,11 +8,12 @@ success, image = vidcap.read()
 count = 0
 while success:
     #LOOK INTO THREADING TO WAIT FOR EACH FRAME TO BE SCANNED CONSECUTIVELY
-    # change to save to /frames ("PATH/frame%d.jpg")
-    cv2.imwrite("/Users/sam/Programming/Image-processing/frames/frame%d.jpg" % count, image)
-    filename = compress("/Users/sam/Programming/Image-processing/frames/frame%d.jpg" % count)
-    rgb = get_color(filename)
+    filename = "/Users/sam/Programming/Image-processing/frames/frame%d.jpg" % count
+    cv2.imwrite(filename, image)
+    if count % 8 == 0:
+        compress(filename)
+        print(f"\nFrame {count}")
+        rgb = get_color(filename)
     os.remove(filename)
-    os.remove("/Users/sam/Programming/Image-processing/frames/frame%d.jpg" % count)
     success, image = vidcap.read()
     count += 1
