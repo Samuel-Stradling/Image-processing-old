56a67c9427c9e67bd874877012119143aadc332d
Macbook air <sammystradling2006@gmail.com>
2022-07-30 19:02:45 +0100

    add get color for each frame

--- video.py
+++ video.py
@@ -8,6 +8,7 @@ while success:
     #LOOK INTO THREADING TO WAIT FOR EACH FRAME TO BE SCANNED CONSECUTIVELY
     # change to save to /frames ("PATH/frame%d.jpg")
     cv2.imwrite("/Users/sam/Programming/Image-processing/frames/frame%d.jpg" % count, image)
+    get_color("/Users/sam/Programming/Image-processing/frames/frame%d.jpg" % count)
     success, image = vidcap.read()
     print("Read a new frame: ", success)
     count += 1
