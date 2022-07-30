42b195e6f44aea33e10eaeadc422f2940f484685
Macbook air <sammystradling2006@gmail.com>
2022-07-30 10:50:53 +0100

    video.py saves frames in wd; to implement get color

---
+++ video.py
@@ -0,0 +1,10 @@
+import cv2
+from color import get_color
+vidcap = cv2.VideoCapture('test.mp4')
+success,image = vidcap.read()
+count = 0
+while success:
+  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
+  success,image = vidcap.read()
+  print('Read a new frame: ', success)
+  count += 1
