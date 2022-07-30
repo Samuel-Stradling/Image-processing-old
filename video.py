4c710c5c5b0b2e96f4c0ecfbd3da69af374245ca
Macbook air <sammystradling2006@gmail.com>
2022-07-30 20:14:49 +0100

    video analysis working but NEED TO LOOK AT THREADING TO SYNC

--- video.py
+++ video.py
@@ -2,18 +2,40 @@ import cv2
 from Vcompression import compress
 from Vcolor import get_color
 import os
+import pygame
 
-vidcap = cv2.VideoCapture("test.mp4")
-success, image = vidcap.read()
-count = 0
-while success:
-    #LOOK INTO THREADING TO WAIT FOR EACH FRAME TO BE SCANNED CONSECUTIVELY
-    filename = "/Users/sam/Programming/Image-processing/frames/frame%d.jpg" % count
-    cv2.imwrite(filename, image)
-    if count % 8 == 0:
-        compress(filename)
-        print(f"\nFrame {count}")
-        rgb = get_color(filename)
-    os.remove(filename)
+#LOOK INTO THREADING TO CONTROL SPEED OF ANALYSIS
+
+pygame.init()
+screen = pygame.display.set_mode((500, 700))
+pygame.display.set_caption("Avg Color")
+clock = pygame.time.Clock()
+done = False
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    clock.tick(60)
+
+    vidcap = cv2.VideoCapture("test.mp4")
     success, image = vidcap.read()
-    count += 1
+    count = 0
+    while success:
+        #LOOK INTO THREADING TO WAIT FOR EACH FRAME TO BE SCANNED CONSECUTIVELY
+        filename = "/Users/sam/Programming/Image-processing/frames/frame%d.jpg" % count
+        cv2.imwrite(filename, image)
+        if count % 8 == 0:
+    
+            compress(filename)
+            print(f"\nFrame {count}")
+            rgb = get_color(filename)
+            screen.fill(rgb)
+            pygame.display.flip()
+
+        os.remove(filename)
+        success, image = vidcap.read()
+        count += 1
+    
+
+pygame.quit()
