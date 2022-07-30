bfa2a6463f82bafa1410764bc97d585a0ee4c98b
Macbook air <sammystradling2006@gmail.com>
2022-07-30 18:35:44 +0100

    changed path to save frames to frames f0lder

--- video.py
+++ video.py
@@ -7,7 +7,7 @@ count = 0
 while success:
     #LOOK INTO THREADING TO WAIT FOR EACH FRAME TO BE SCANNED CONSECUTIVELY
     # change to save to /frames ("PATH/frame%d.jpg")
-    cv2.imwrite("frame%d.jpg" % count, image)
+    cv2.imwrite("/Users/sam/Programming/Image-processing/frames/frame%d.jpg" % count, image)
     success, image = vidcap.read()
     print("Read a new frame: ", success)
     count += 1
