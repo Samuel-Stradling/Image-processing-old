e7c857f97fec02ddca33baead32728decf6431b4
Macbook air <sammystradling2006@gmail.com>
2022-07-30 19:16:14 +0100

    use compression, optimised get color, delete frames and cframes after analysis

--- video.py
+++ video.py
@@ -1,5 +1,7 @@
 import cv2
-from color import get_color
+from compression import compress
+from colorForVideo import get_color
+import os
 
 vidcap = cv2.VideoCapture("test.mp4")
 success, image = vidcap.read()
@@ -8,7 +10,9 @@ while success:
     #LOOK INTO THREADING TO WAIT FOR EACH FRAME TO BE SCANNED CONSECUTIVELY
     # change to save to /frames ("PATH/frame%d.jpg")
     cv2.imwrite("/Users/sam/Programming/Image-processing/frames/frame%d.jpg" % count, image)
-    get_color("/Users/sam/Programming/Image-processing/frames/frame%d.jpg" % count)
+    filename = compress("/Users/sam/Programming/Image-processing/frames/frame%d.jpg" % count)
+    rgb = get_color(filename)
+    os.remove(filename)
+    os.remove("/Users/sam/Programming/Image-processing/frames/frame%d.jpg" % count)
     success, image = vidcap.read()
-    print("Read a new frame: ", success)
     count += 1
