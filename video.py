281ba2ebcc5210878834b2514919fb2aeaed55b6
Macbook air <sammystradling2006@gmail.com>
2022-07-30 20:18:23 +0100

    removed legacy video code

--- video.py
+++ video.py
@@ -11,6 +11,7 @@ screen = pygame.display.set_mode((500, 700))
 pygame.display.set_caption("Avg Color")
 clock = pygame.time.Clock()
 done = False
+
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -19,7 +20,6 @@ while not done:
     clock.tick(60)
 
     vidcap = cv2.VideoCapture("test.mp4")
-    success, image = vidcap.read()
     count = 0
     while success:
         #LOOK INTO THREADING TO WAIT FOR EACH FRAME TO BE SCANNED CONSECUTIVELY
