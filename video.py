96af8b83a7a403c4f5587bf104364a0fc21c294b
Macbook air <sammystradling2006@gmail.com>
2022-07-30 11:02:06 +0100

    added threading comments

--- video.py
+++ video.py
@@ -5,6 +5,7 @@ vidcap = cv2.VideoCapture("test.mp4")
 success, image = vidcap.read()
 count = 0
 while success:
+    #LOOK INTO THREADING TO WAIT FOR EACH FRAME TO BE SCANNED CONSECUTIVELY
     # change to save to /frames ("PATH/frame%d.jpg")
     cv2.imwrite("frame%d.jpg" % count, image)
     success, image = vidcap.read()
