03e1329640315ffe832edfaf37e5c11a8cdd40c9
Macbook air <sammystradling2006@gmail.com>
2022-08-05 22:43:50 +0200

    made video.py work with playing video

--- video.py
+++ video.py
@@ -5,46 +5,48 @@ import os
 import pygame
 import time
 
-#LOOK INTO THREADING TO CONTROL SPEED OF ANALYSIS
-def video_color():
-    pygame.init()
-    screen = pygame.display.set_mode((500, 700))
-    pygame.display.set_caption("Avg Color")
-    clock = pygame.time.Clock()
-    done = False
 
-    while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
+def frame_color(frame):
+    filename = "/Users/sam/Programming/Image-processing/frames/frame.jpg"
+    cv2.imwrite(filename, frame)
 
-        clock.tick(60)
+    compress(filename)
+    rgb = get_color(filename)
+    screen.fill(rgb)
+    pygame.display.flip()
 
-        vidcap = cv2.VideoCapture("test.mp4")
-        success, image = vidcap.read()
-        count = 0
-        while success:
-            #LOOK INTO THREADING TO WAIT FOR EACH FRAME TO BE SCANNED CONSECUTIVELY
-            filename = "/Users/sam/Programming/Image-processing/frames/frame%d.jpg" % count
-            cv2.imwrite(filename, image)
-            if count % 5 == 0:
-        
-                compress(filename)
-                print(f"\nFrame {count}")
-                rgb = get_color(filename)
-                screen.fill(rgb)
-                pygame.display.flip()
+    os.remove(filename)
 
-            os.remove(filename)
-            success, image = vidcap.read()
-            count += 1
-            if count == 0:
-                success = False
+
+def play_video():
+    cap = cv2.VideoCapture("test3.mp4")
+    frameNo = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        frameNo += 1
+        if ret:
+            cv2.imshow("frame", frame)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
-        if success == False:
-            time.sleep(5)
+            if frameNo % 4 == 0:
+                print(frameNo)
+                # time.sleep(1 / 60)
+                frame_color(frame)
+        else:
             break
-        
+    cap.release()
+    cv2.destroyAllWindows()
 
-    pygame.quit()
-video_color()
+
+pygame.init()
+screen = pygame.display.set_mode((500, 700))
+pygame.display.set_caption("Avg Color")
+clock = pygame.time.Clock()
+done = False
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    play_video()
