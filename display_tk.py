ccb2b4ab829198b7023f8c2102c43524ffca3db5
Macbook air <sammystradling2006@gmail.com>
2022-08-06 18:44:29 +0200

    readability improvements

--- display_tk.py
+++ display_tk.py
@@ -7,34 +7,47 @@ import os
 
 
 def _from_rgb(rgb):
-    """translates an rgb tuple of int to a tkinter friendly color code"""
+
+    """translates an rgb tuple of int to a tkinter friendly color code (HEX)"""
+
     r, g, b = rgb
+
     return f"#{r:02x}{g:02x}{b:02x}"
 
 
+# initialising tkinter window
 window = Tk()
-
-
 size = "900x500"
 window.geometry(size)
 window.title("Average Color")
 
 
 def select_file():
-    """Selects file using tkinter gui"""
-    filetypes = (("image files", "*.jpg"), ("All files", "*.*"), ("image files", "*.png"))
 
+    """Selects file using tkinter gui"""
+
+    filetypes = (
+        ("image files", "*.jpg"),
+        ("All files", "*.*"),
+        ("image files", "*.png"),
+    )
+
+    # starts a file open dialog
     filename = fd.askopenfilename(
         title="Open a file", initialdir="/", filetypes=filetypes
     )
 
-    filename = compress(filename) 
+    filename = compress(filename)
     get_change_color(filename)
-    
 
 
 def get_change_color(directory):
-    """Gets rid of the labels and updates background color"""
+
+    """calls the get_color function with the selected image.
+    Also removes the widgets in order to display the new background color with
+    a plain background. Is a chain of functions becuase the button action can only accept one method as
+    the command"""
+
     color = get_color(directory)
     textLabel.pack_forget()
     open_button.pack_forget()
@@ -42,7 +55,7 @@ def get_change_color(directory):
     os.remove(directory)
 
 
-
+# widgets made global to reduce bloating the functions with extra parameters
 global textLabel
 textLabel = Label(
     window, text="Find the average color of an image", font=("Arial", 25, "bold")
@@ -51,7 +64,7 @@ textLabel.pack(pady=15)
 
 global open_button
 open_button = ttk.Button(window, width=35, text="Open a File", command=select_file)
-#can only have one function for command, so select_file hosts change color
+# can only have one function for command, so select_file hosts change color
 open_button.pack()
 
 
