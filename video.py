ef3731f1e16a09e1233180d4a6a54a30aa1de140
Macbook air <sammystradling2006@gmail.com>
2022-08-06 18:05:39 +0200

    made readability improvements

--- video.py
+++ video.py
@@ -7,26 +7,45 @@ import time
 
 
 def frame_color(frame):
+
+    """Gets the color for each frame using the optimised get_color method from Vcolor"""
+
     filename = "/Users/sam/Programming/Image-processing/frames/frame.jpg"
+
+    # saves the frame
     cv2.imwrite(filename, frame)
-
+    # overwrites the file with the scaled down version of itself
     compress(filename)
-    rgb = get_color(filename)
-    screen.fill(rgb)
-    pygame.display.flip()
 
+    rgb = get_color(filename)
+    # pygame fill the screen with the received average rgb
+    screen.fill(rgb)
+    # update the screen
+    pygame.display.flip()
+    # remove the saved frame
     os.remove(filename)
 
 
 def play_video():
+
+    """This plays all frames of the video (using cv2) and at fourth frame, gets the average color
+    of the frame which is displayed with pygame.
+    Scanning every frame is too slow so this does multiples [of 4]"""
+
     cap = cv2.VideoCapture("test2.mp4")
+
+    # counter for the frames to allow for scanning of multiples of frames instead of each and every
     frameNo = 0
+
     while cap.isOpened():
         ret, frame = cap.read()
 
         frameNo += 1
+        # if cap.read() is returning:
         if ret:
-            cv2.imshow("frame", frame)
+            # display that frame
+            cv2.imshow("Video", frame)
+            # 'q' to quit
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
             if frameNo % 4 == 0:
@@ -39,9 +58,10 @@ def play_video():
     cv2.destroyAllWindows()
 
 
+# start of the actual code initing pygame
 pygame.init()
-screen = pygame.display.set_mode((500, 700))
-pygame.display.set_caption("Avg Color")
+screen = pygame.display.set_mode((300, 300))
+pygame.display.set_caption("Average Color")
 clock = pygame.time.Clock()
 done = False
 
@@ -49,4 +69,5 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+    # calls play_video which calls get_color when neccessary
     play_video()
