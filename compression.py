4674eb003cfb91ffee458e646effafe2935320da
Macbook air <sammystradling2006@gmail.com>
2022-08-06 18:32:25 +0200

    readability improvements

--- compression.py
+++ compression.py
@@ -1,17 +1,27 @@
 from PIL import Image
+
+
 def compress(directory):
-    # set the base width of the result
-    widthRescale = 300 #change to control rescaling of image
+
+    """resizes the given image and saves as C_image.jpg. Returns the new directory"""
+
+    # set the base width of the image
+    widthRescale = 300
     img = Image.open(directory)
-    # determining the height ratio
-    wpercent = (widthRescale/float(img.size[0]))
-    hsize = int((float(img.size[1])*float(wpercent)))
+    # determining the height ratio to maintain the aspect ratio
+    wpercent = widthRescale / float(img.size[0])
+    hsize = int((float(img.size[1]) * float(wpercent)))
     # resize image and save
-    img = img.resize((widthRescale,hsize), Image.Resampling.LANCZOS)
+    img = img.resize((widthRescale, hsize), Image.Resampling.LANCZOS)
+    # find index of final occurence of slash
     lastslash = directory.rfind("/") + 1
+
+    # saves the compressed file preceded by C
     directory = list(directory)
     directory.insert(lastslash, "C")
-    directory = ''.join(directory)
+    directory = "".join(directory)
     img.save(directory)
     return directory
-#compress("/Users/sam/Pictures/mac tonight/2.jpg")
+
+
+# compress("/Users/sam/Pictures/mac tonight/2.jpg")
